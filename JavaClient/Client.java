1c46f5a162793f753629326e114647633e1c8a34
Audrey Dennis <41072545+audreydennis@users.noreply.github.com>
2023-05-10 20:04:05 -0700

    Almost Finished

--- JavaClient/Client.java
+++ JavaClient/Client.java
@@ -4,6 +4,7 @@ import java.net.http.*;
 import javax.xml.parsers.*;
 import javax.xml.xpath.*;
 import org.w3c.dom.*;
+import java.net.http.HttpResponse.BodyHandlers;
 
 import org.w3c.dom.Node;
 
@@ -11,38 +12,136 @@ import org.w3c.dom.Node;
  * This approach uses the java.net.http.HttpClient classes, which
  * were introduced in Java11.
  */
+
 public class Client {
     private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
     public static void main(String... args) throws Exception {
-        System.out.println(add() == 0);
-        System.out.println(add(1, 2, 3, 4, 5) == 15);
-        System.out.println(add(2, 4) == 6);
-        System.out.println(subtract(12, 6) == 6);
-        System.out.println(multiply(3, 4) == 12);
-        System.out.println(multiply(1, 2, 3, 4, 5) == 120);
-        System.out.println(divide(10, 5) == 2);
-        System.out.println(modulo(10, 5) == 0);
+        String server = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        String userAgent = "GroupName"; // confused as to what this is supposed to be
+
+        Object subtractResult = makeXmlRpcRequest(httpClient, server, port, userAgent, "subtract", 12, 6);
+        System.out.println("subtract(12, 6) = " + subtractResult);
+
+        Object multiplyResult = makeXmlRpcRequest(httpClient, server, port, userAgent, "multiply", 3, 4);
+        System.out.println("multiply(3, 4) = " + multiplyResult);
+
+        Object divideResult = makeXmlRpcRequest(httpClient, server, port, userAgent, "divide", 10, 2);
+        System.out.println("divide(10, 2) = " + divideResult);
+
+        Object moduloResult = makeXmlRpcRequest(httpClient, server, port, userAgent, "modulo", 10, 5);
+        System.out.println("modulo(10, 5) = " + moduloResult);
+
+        Object addZeroResult = makeXmlRpcRequest(httpClient, server, port, userAgent, "add", 0);
+        System.out.println("add(0) = " + addZeroResult);
+
+        Object addResult = makeXmlRpcRequest(httpClient, server, port, userAgent, "add", 1, 2, 3, 4, 5);
+        System.out.println("add(1, 2, 3, 4, 5) = " + addResult);
+
+        Object multiplyMultipleResult = makeXmlRpcRequest(httpClient, server, port, userAgent, "multiply", 1, 2, 3, 4, 5);
+        System.out.println("multiply(1, 2, 3, 4, 5) = " + multiplyMultipleResult);
     }
-    public static int add(int lhs, int rhs) throws Exception {
-        return -1;
+
+    private static Object makeXmlRpcRequest(HttpClient httpClient, String server, int port, String userAgent, String methodName, Object... params) throws Exception {
+        String xmlRequest = buildXmlRpcRequest(methodName, params);
+        // System.out.println("Request XML");
+        // checkingRequest(xmlRequest);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://" + server + ":" + port + "/RPC"))
+                .header("Content-Type", "text/xml")
+                .header("User-Agent", userAgent)
+                .POST(HttpRequest.BodyPublishers.ofString(xmlRequest))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+
+        String responseXml=response.body();
+        System.out.println("Response XML");
+        checkingRequest(responseXml);
+
+        Object result = parseXmlRpcResponse(response.body());
+        return result;
     }
-    public static int add(Integer... params) throws Exception {
-        return -1;
+
+    private static String buildXmlRpcRequest(String methodName, Object... params) {
+        // Build the XML-RPC request payload
+        StringBuilder xmlBuilder = new StringBuilder();
+        xmlBuilder.append("<?xml version=\"1.0\"?>");
+        xmlBuilder.append("<methodCall>");
+        xmlBuilder.append("<methodName>").append(methodName).append("</methodName>");
+        if (params.length > 0) {
+            xmlBuilder.append("<params>");
+            for (Object param : params) {
+                xmlBuilder.append("<param>");
+                xmlBuilder.append("<value>").append(getXmlRpcValue(param)).append("</value>");
+                xmlBuilder.append("</param>");
+            }
+            xmlBuilder.append("</params>");
+        }
+        xmlBuilder.append("</methodCall>");
+
+        return xmlBuilder.toString();
     }
-    public static int subtract(int lhs, int rhs) throws Exception {
-        return -1;
+
+    private static String getXmlRpcValue(Object value) {
+        // if (value instanceof Integer) {
+            return "<i4>" + value + "</i4>";
+        // } else {
+            // return "<string>" + value + "</string>";
+        // }
     }
-    public static int multiply(int lhs, int rhs) throws Exception {
-        return -1;
+
+    private static Object parseXmlRpcResponse(String xmlResponse) throws Exception {
+        try {
+            BufferedReader reader = new BufferedReader(new StringReader(xmlResponse));
+            String line;
+            String faultCode="";
+
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("<i4>")) {
+                    int start = line.indexOf("<i4>") + 4;
+                    int end = line.indexOf("</i4>");
+                    String value = line.substring(start, end);
+                    int intValue = Integer.parseInt(value);
+                    return intValue;
+                }
+                if (line.contains("faultCode")) {
+                    int start = line.indexOf("<int>") + 5;
+                    int end = line.indexOf("</int>");
+                    String value = line.substring(start, end);
+                    faultCode= "Fault Code: " + Integer.parseInt(value);
+                }
+                if (line.contains("faultString")) {
+                    int start = line.indexOf("<string>") + 8;
+                    int end = line.indexOf("</string>");
+                    String value = line.substring(start, end);
+                    return faultCode + " FaultString: " + value;
+                }
+            }
+            return null;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-    public static int multiply(Integer... params) throws Exception {
-        return -1;
-    }
-    public static int divide(int lhs, int rhs) throws Exception {
-        return -1;
-    }
-    public static int modulo(int lhs, int rhs) throws Exception {
-        return -1;
+
+    // Printing the XML so i can check format
+    private static void checkingRequest(String xmlResponse) throws Exception {
+        try {
+            BufferedReader reader = new BufferedReader(new StringReader(xmlResponse));
+            String line;
+            // Read each line of the XML request
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
+++ JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
@@ -2,27 +2,193 @@ package edu.uw.info314.xmlrpc.server;
 
 import java.util.*;
 import java.util.logging.*;
+import java.io.*;
 
 import static spark.Spark.*;
 
+
 class Call {
     public String name;
-    public List<Object> args = new ArrayList<Object>();
+    public List < Object > args = new ArrayList < Object > ();
 }
 
 public class App {
     public static final Logger LOG = Logger.getLogger(App.class.getCanonicalName());
 
     public static void main(String[] args) {
-        LOG.info("Starting up on port 4567");
+        LOG.info("Starting up on port 8080");
 
-        // This is the mapping for POST requests to "/RPC";
-        // this is where you will want to handle incoming XML-RPC requests
-        post("/RPC", (request, response) -> { response.status(500); return "TBD"; });
+        port(8080);
 
-        // Each of the verbs has a similar format: get() for GET,
-        // put() for PUT, delete() for DELETE. There's also an exception()
-        // for dealing with exceptions thrown from handlers.
-        // All of this is documented on the SparkJava website (https://sparkjava.com/).
+        post("/RPC", (request, response) -> {
+
+            if (!request.requestMethod().equals("POST")) {
+                response.status(405);
+                return "Method Not Supported"; 
+            }
+
+            if (!request.uri().equals("/RPC")) {
+                response.status(404);
+                return "Not Found";
+            }
+
+            String host = request.host();
+            response.header("Host", host);
+
+            String xmlRequest = request.body();
+
+            Calc calc = new Calc(); 
+            Call call = new Call(); 
+
+            String methodName = getMethodName(xmlRequest);
+            LOG.info("methodName: " + methodName);
+            call.name = methodName; 
+
+            List < Object > i4Values = geti4(xmlRequest);
+            if (i4Values.stream().anyMatch(arg -> !(arg instanceof Integer))) {
+                response.type("text/xml");
+                return buildXmlFaultResponse(3, "illegal argument type");
+            }else{
+            System.out.println("i4Values: " + i4Values);
+            call.args = i4Values; 
+            int result = 11;
+
+            if ("add".equals(call.name)) {
+                result = calc.add(convertArgsToIntArray(call.args));
+            } else if ("subtract".equals(call.name)) {
+                int lhs = (int) call.args.get(0);
+                int rhs = (int) call.args.get(1);
+                result = calc.subtract(lhs, rhs);
+            } else if ("multiply".equals(call.name)) {
+                result = calc.multiply(convertArgsToIntArray(call.args));
+            } else if ("divide".equals(call.name)) {
+                int lhs = (int) call.args.get(0);
+                int rhs = (int) call.args.get(1);
+                if(rhs==0){
+                    response.type("text/xml");
+                    return buildXmlFaultResponse(1, "divide by zero");
+                }else{
+                    result = calc.divide(lhs, rhs);
+                }
+                
+            } else if ("modulo".equals(call.name)) {
+                int lhs = (int) call.args.get(0);
+                int rhs = (int) call.args.get(1);
+                if(rhs==0){
+                    response.type("text/xml");
+                    return buildXmlFaultResponse(1, "divide by zero");
+                }else{
+                    result = calc.modulo(lhs, rhs);
+                }
+            } else {
+                result = 0;
+            }
+
+            String xmlResponse = buildXmlResponse(methodName, result); //testing to get response
+
+            response.type("text/xml");
+            return xmlResponse;
+            }
+        });
+    }
+
+    public static List < Object > geti4(String xmlRequest) {
+        List < Object > i4Values = new ArrayList < > ();
+
+        int startIndex = 0;
+        int endIndex;
+
+        while ((startIndex = xmlRequest.indexOf("<i4>", startIndex)) != -1) {
+            startIndex += 4; 
+            endIndex = xmlRequest.indexOf("</i4>", startIndex);
+
+            if (endIndex != -1) {
+                String value = xmlRequest.substring(startIndex, endIndex);
+
+                try {
+                    int intValue = Integer.parseInt(value);
+                    i4Values.add(intValue);
+                } catch (NumberFormatException e) {
+                    return null; 
+                }
+
+                startIndex = endIndex + 5; 
+            } else {
+                break; 
+            }
+        }
+        return i4Values;
+    }
+
+    public static String getMethodName(String xmlRequest) {
+        try {
+            BufferedReader reader = new BufferedReader(new StringReader(xmlRequest));
+            String line;
+
+            // Read each line of the XML request until methodName
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("<methodName>")) {
+                    int start = line.indexOf("<methodName>") + 12;
+                    int end = line.indexOf("</methodName>");
+                    String value = line.substring(start, end);
+                    return value;
+                }
+            }
+            return null;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //build same XML as Client
+    private static String buildXmlResponse(String methodName, int result) {
+        StringBuilder xmlBuilder = new StringBuilder();
+        xmlBuilder.append("<?xml version=\"1.0\"?>");
+        xmlBuilder.append("<methodResponse>");
+        xmlBuilder.append("<params>");
+        xmlBuilder.append("<param>");
+        xmlBuilder.append("<value>").append(getXmlRpcValue(result)).append("</value>");
+        xmlBuilder.append("</param>");
+        xmlBuilder.append("</params>");
+        xmlBuilder.append("</methodResponse>");
+
+        return xmlBuilder.toString();
+    }
+
+    private static String buildXmlFaultResponse(int faultCode, String faultString) {
+        StringBuilder xmlBuilder = new StringBuilder();
+        xmlBuilder.append("<?xml version=\"1.0\"?>");
+        xmlBuilder.append("<methodResponse>");
+        xmlBuilder.append("<fault>");
+        xmlBuilder.append("<value>");
+        xmlBuilder.append("<struct>");
+        xmlBuilder.append("<member>");
+        xmlBuilder.append("<name>faultCode</name>");
+        xmlBuilder.append("<value><int>").append(faultCode).append("</int></value>");
+        xmlBuilder.append("</member>");
+        xmlBuilder.append("<member>");
+        xmlBuilder.append("<name>faultString</name>");
+        xmlBuilder.append("<value><string>").append(faultString).append("</string></value>");
+        xmlBuilder.append("</member>");
+        xmlBuilder.append("</struct>");
+        xmlBuilder.append("</value>");
+        xmlBuilder.append("</fault>");
+        xmlBuilder.append("</methodResponse>");
+
+        return xmlBuilder.toString();
+    }
+
+    private static String getXmlRpcValue(int value) {
+        return "<i4>" + value + "</i4>";
+    }
+
+    private static int[] convertArgsToIntArray(List < Object > args) {
+        int[] intArray = new int[args.size()];
+        for (int i = 0; i < args.size(); i++) {
+            intArray[i] = (int) args.get(i);
+        }
+        return intArray;
     }
 }
